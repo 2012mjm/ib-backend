74af1495d81265e94030a544701b5446521e85b0
Unknown <2007mjm@gmail.com>
2018-12-08 18:24:55 +0330

    migrate to isAuthorized sails v1

--- api/policies/isAuthorized.js
+++ api/policies/isAuthorized.js
@@ -5,31 +5,31 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Policies
  */
 
-module.exports = function (req, res, next) {
-  var token;
+module.exports = async (req, res, next) => {
+  let token;
 
   if (req.headers && req.headers.authorization) {
-    var parts = req.headers.authorization.split(' ');
+    let parts = req.headers.authorization.split(' ');
     if (parts.length == 2) {
-      var scheme = parts[0],
+      let scheme = parts[0],
         credentials = parts[1];
 
       if (/^Bearer$/i.test(scheme)) {
         token = credentials;
       }
     } else {
-      return res.json(401, {err: 'Format is Authorization: Bearer [token]'});
+      return res.status(401).json({_errors: ['Format is Authorization: Bearer [token]']})
     }
   } else if (req.param('token')) {
     token = req.param('token');
     // We delete the token from param to not mess with blueprints
     delete req.query.token;
   } else {
-    return res.json(401, {_errors: ['No Authorization header was found']});
+    return res.status(401).json({_errors: ['No Authorization header was found']})
   }
 
-  JwtService.verify(token, function (err, token) {
-    if (err) return res.json(401, {_errors: ['Invalid Token!']});
+  JwtService.verify(token, (err, token) => {
+    if (err) return res.status(401).json({_errors: ['Invalid Token!']})
     req.token = token; // This is the decrypted token or the payload you provided
     next();
   });
